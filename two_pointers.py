872ec964826523adf485b347e69908a97b55c43b
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-01 13:30:19 +0800

    Is Palindrome

--- two_pointers.py
+++ two_pointers.py
@@ -1,1 +1,29 @@
+# Two Pointers Solutions
+from collections import Counter, defaultdict
 
+class Solutions:
+	# Solution Functions
+	
+	def isPalindrome(self, s: str) -> bool:
+		'''Check if a string is a palindrome
+
+  		Space Complexity: O(n) -> Cleaned string
+    		Time Complexity: O(n) -> Iteration to half the string
+
+      		Args:
+			s (str): String
+
+   		Returns:
+     			is_pal (bool): Palindrome check
+		'''
+	        s = [c.lower() for c in s if c.isalnum()]
+        	l, r = 0, len(s) - 1
+		
+        	while l < r:
+            		if s[l] == s[r]:
+		                l += 1
+		                r -= 1
+		        else:
+		        	return False
+       		
+		return True
