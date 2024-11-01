b2f7494042397e2d6c22384dbd9402aa0755d2db
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-01 16:33:11 +0800

    Three Sum

--- two_pointers.py
+++ two_pointers.py
@@ -51,3 +51,41 @@ class Solutions:
             		else:
                 		r -= 1
 		return [-1, -1]
+
+	def threeSum(self, nums: List[int]) -> List[List[int]]:
+		'''Get all sets of 3 elements that sum to 0
+
+  		Space Complexity: O(n) -> Sorting
+    		Time Complexity: O(n^2) -> Two Sum for each element
+
+      		Args:
+			nums (list): Iterable of numbers
+
+   		Returns:
+     			res (list): List of solutions
+		'''
+        	res = []
+        	nums.sort()
+
+        	for i, n in enumerate(nums):
+			# Conditions to exit / skip iteration
+            		if n > 0:
+                		break
+            		if i > 0 and n == nums[i-1]:
+                		continue
+            
+            		l, r = i + 1, len(nums) - 1
+			while l < r:
+                		threesum = n + nums[l] + nums[r]
+
+                		if threesum == 0:
+                    			res.append([n, nums[l], nums[r]])
+                    			l += 1
+                    			r -= 1
+                    			while nums[l] == nums[l-1] and l < r:
+                        			l += 1
+                		elif threesum < 0:
+                    			l += 1
+                		else:
+                    			r -= 1
+        	return res
