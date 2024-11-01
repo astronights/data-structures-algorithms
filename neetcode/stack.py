275e33de136bebe2742dd60c08579210631cb285
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-01 20:40:17 +0800

    Min Stack

--- neetcode/stack.py
+++ neetcode/stack.py
@@ -28,3 +28,55 @@ class Solutions:
             		else:
                 		stack.append(c)
 		return len(stack) == 0
+
+
+class MinStack:
+
+	def __init__(self):
+		'''Minimum Stack
+
+		Space Complexity: O(n) -> Stack Elements
+     		'''
+	    self.stack = []
+	    self.min_stack = []
+
+   	 def push(self, val: int) -> None:
+		'''Add element to stack
+
+      		Time Complexity: O(1) -> List Append
+
+ 		Args:
+   			val (int): Value to append
+      		'''
+		min_val = val if not self.min_stack else min(self.min_stack[-1], val)
+     		
+        	self.stack.append(val) 
+	        self.min_stack.append(min_val)
+
+	def pop(self) -> None:
+		'''Pop top element from stack
+
+    		Time Complexity: O(1) -> List Pop
+      		'''
+	        self.stack.pop()
+        	self.min_stack.pop()
+
+    	def top(self) -> int:
+		'''Get top element from stack
+
+  		Time Complexity: O(1) -> List Access
+
+    		Returns:
+      			val (int): Top Element
+	 	'''
+        	return self.stack[-1]
+
+    	def getMin(self) -> int:
+		'''Get minimum element from stack
+
+  		Time Complexity: O(1) -> List Access
+
+    		Returns:
+      			val (int): Minimum Element
+	 	'''
+        	return self.min_stack[-1]
