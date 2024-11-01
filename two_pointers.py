bf40cf0dc9f74d7a856f9fb8a8ecd364cf18e52c
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-01 13:35:02 +0800

    Two sum sorted

--- two_pointers.py
+++ two_pointers.py
@@ -25,5 +25,29 @@ class Solutions:
 		                r -= 1
 		        else:
 		        	return False
-       		
 		return True
+
+	def twoSum(self, numbers: List[int], target: int) -> List[int]:
+		'''Get indices of two sum in sorted array
+
+  		Space Complexity: O(1) -> Pointers
+    		Time Complexity: O(n) -> Iteration over elements
+
+      		Args:
+			numbers (list): Iterable of numbers
+   			target (int): Target sum
+
+      		Returns:
+			indices (list): Indices of elements
+   		'''
+        	l, r = 0, len(numbers) - 1
+        
+		while l < r:
+			cur_sum = numbers[l] + numbers[r]
+            		if cur_sum == target:
+                		return [l+1, r+1]
+            		elif cur_sum < target:
+                		l += 1
+            		else:
+                		r -= 1
+		return [-1, -1]
