164d73f8fffe72f6c86e3a2a9c872f5c6013366c
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-02 13:56:42 +0800

    Binary Search

--- neetcode/binary_search.py
+++ neetcode/binary_search.py
@@ -1,1 +1,30 @@
+# Binary Search Solutions
 
+class Solutions:
+	# Solution Functions
+
+        def search(self, nums: List[int], target: int) -> int:
+                '''Binary Search in a list
+
+                Space Complexity: O(1) -> Pointers
+                Time Complexity: O(log n) -> Binary Search
+
+                Args:
+                        nums (list): Iterable of numbers
+                        target (int): Number to find
+
+                Returns:
+                        mid (int): Index of solution
+                '''
+                l, r = 0, len(nums) - 1
+
+                while l <= r:
+                        mid = (l + r) // 2
+
+                        if nums[mid] == target:
+                                return mid
+                        elif nums[mid] < target:
+                                l = mid + 1
+                        else:
+                                r = mid - 1
+                return -1
