c40ada9c6e340fa0cb269ac94cc47a9a76438cc6
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-04 14:05:22 +0800

    Longest substring without duplicates

--- neetcode/sliding_window.py
+++ neetcode/sliding_window.py
@@ -23,3 +23,30 @@ class Solutions:
             		max_profit = max(max_profit, price - min_price)
             		min_price = min(min_price, price)
         	return max_profit 
+
+	def lengthOfLongestSubstring(self, s: str) -> int:
+		'''Length of longest substring without duplicates
+
+  		Space Complexity: O(n) -> Frequency dictionary
+    		Time Complexity: O(n) -> Single Pass over characters
+
+      		Args:
+			s (str): Iterable of characters
+
+   		Returns:
+     			longest (int): Length of longest string
+		'''
+        	start = 0
+        	longest = 0
+		seen = defaultdict(int)
+		
+        	for i, c in enumerate(s):
+            		if seen.get(c, 0) == 0:
+                		seen[c] += 1
+                		longest = max(longest, i - ix + 1)
+            		else:
+                		while seen.get(c) != 0:
+                    			seen[s[ix]] -= 1
+                    			ix += 1
+                		seen[c] = 1
+        	return longest
