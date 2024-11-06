b840bf1ff384932e1c352e2b94da06f509ccc258
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-06 19:37:48 +0800

    Merge linked lists.

--- neetcode/linked_list.py
+++ neetcode/linked_list.py
@@ -34,3 +34,39 @@ class Solutions:
             		prev = cur
             		cur = nxt
         	return prev
+
+	def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+		'''Merge two sorted linked lists
+
+  		Space Complexity: O(1) -> Linked List Pointers
+    		Time Complexity: O(m+n) -> Single pass over both lists
+
+      		Values: List sizes (m, n)
+
+ 		Args:
+   			list1 (ListNode): Linked List 1
+      			list2 (ListNode): Linked List 2
+
+  		Returns:
+    			res (ListNode): Merged Linked List
+       		'''
+        	res = ListNode()
+        	dummy = res
+        
+		while list1 and list2:
+            		if list1.val < list2.val:
+                		dummy.next = list1
+                		list1 = list1.next
+            		else:
+                		dummy.next = list2
+                		list2 = list2.next
+            		dummy = dummy.next
+        
+		if list1:
+            		dummy.next = list1
+        	if list2:
+            		dummy.next = list2
+        	
+		return res.next
+
+        
