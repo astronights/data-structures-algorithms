ee98e75e23059f4b450339c00b67a619dd370a54
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-08 18:17:41 +0800

    Diameter of tree

--- neetcode/trees.py
+++ neetcode/trees.py
@@ -49,3 +49,38 @@ class Solutions:
         	if not root:
             		return 0
         	return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+	def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+		'''Diameter of Binary Tree
+
+  		Space Complexity: O(n) -> Tree Structure
+    		Time Complexity: O(n) -> Node iteration
+
+      		Args:
+			root (TreeNode): Binary Tree
+
+   		Returns:
+     			max_D (int): Diameter
+		'''
+        	max_D = 0
+        
+		def dfs(tree: Optional[TreeNode]) -> int:
+			'''DFS Through Tree
+
+      			Args:
+	 			tree (TreeNode): Tree Node
+     			Returns:
+				height (int): Height so far
+    			'''
+            		nonlocal max_D
+            		
+			if not tree:
+                		return 0
+            		left_D = dfs(tree.left)
+            		right_D = dfs(tree.right)
+
+            		max_D = max(max_D, left_D + right_D)
+            		return 1 + max(left_D, right_D)
+
+        	_ = dfs(root)
+        	return max_D
