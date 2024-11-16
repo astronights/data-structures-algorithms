620484aa88be0cdb71fe0da203ae691be3565602
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-16 20:52:24 +0800

    Combination Sum

--- neetcode/backtracking.py
+++ neetcode/backtracking.py
@@ -31,3 +31,38 @@ def Solution:
 
         	dfs([], 0)
         	return subs
+
+	def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+		'''Possible combinations that sum to target
+
+  		Space Complexity: O(n * 2 ^ n) -> Power Set
+    		Time Complexity: O(n * 2 ^ n) -> Power Set
+
+      		Args:
+			nums (list): Iterable of numbers
+   			target (int): Sum target
+
+   		Returns:
+     			subs (list): All Possible Subsets
+   		'''
+        	nums.sort()
+        	subs = []
+        
+		def dfs(cur: list, diff: int, ix: int):
+			'''DFS through values
+
+   			Args:
+      				cur (list): Current subset
+	  			diff (int): Remaining difference
+	  			ix (int): Remaining index on list
+			'''
+            		if diff == 0:
+                		subs.append(cur)
+                		return
+            		for i, num in enumerate(nums[ix:]):
+                		if num > diff:
+                    			break
+                		dfs(cur + [num, ], diff - num, ix + i)
+        	
+		dfs([], target, 0)
+        	return subs
