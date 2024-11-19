b0a9acc1dd0754877e11109a3e8208e2196f41dc
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-19 19:56:28 +0800

    Maximum area of island.

--- neetcode/graphs.py
+++ neetcode/graphs.py
@@ -46,3 +46,47 @@ class Solution:
                                 				island.append((nx, ny))
         	return n_isles
 
+
+	def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+		'''Maximum area of island
+
+  		Space Complexity: O(m * n) -> Grid shape
+    		Time Complexity: O(m * n) -> BFS
+
+      		Values: Grid dimensions (m x n)
+
+  		Args:
+    			grid (list): Grid
+
+       		Returns:
+	 		max_area (int): Maximum area of island
+    		'''
+        	max_area = 0
+        	seen = set()
+        	R, C = len(grid), len(grid[0])
+        	dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
+		for i in range(R):
+            		for j in range(C):
+                		if grid[i][j] == 1:
+                    			cur_area = 0
+                    			queue = deque([(i, j)])
+
+					# BFS
+					while queue:
+                        			x, y = queue.popleft()
+                        			if (x, y) in seen:
+                            				continue
+                        			cur_area += 1
+                        			seen.add((x, y))
+
+                        			for dx, dy in dirs:
+                            				nx, ny = x + dx, y + dy
+
+                            				if (nx >= 0 and nx < R 
+							    and ny >= 0 and ny < C 
+							    and grid[nx][ny] == 1):
+                                				queue.append((nx, ny))
+
+                    			max_area = max(max_area, cur_area)
+		return max_area
