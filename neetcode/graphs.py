5175bf14444d7addf40ad7ab3fda8bde57c5e9af
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-19 20:34:53 +0800

    Rotting Fruits

--- neetcode/graphs.py
+++ neetcode/graphs.py
@@ -173,4 +173,52 @@ class Solution:
 			    and grid[nx][ny] == 2147483647):
                     		queue.append((nx, ny, d + 1))
 
+	def orangesRotting(self, grid: List[List[int]]) -> int:
+		'''Time to rot all fruits
+
+  		Space Complexity: O(m * n) -> Grid shape
+    		Time Complexity: O(m * n) -> BFS
+
+      		Values: Grid dimensions (m x n)
+
+ 		Args:
+   			grid (list): Grid of fruits
+
+      		Returns:
+			max_d (int): Time to rot
+   		'''
+        	n_fresh = 0
+		queue = deque()
+        	R, C = len(grid), len(grid[0])
         
+        	for i in range(R):
+            		for j in range(C):
+                		if grid[i][j] == 1:
+                    			n_fresh += 1
+                		elif grid[i][j] == 2:
+                    			queue.append((i, j, 0))
+
+		max_d = 0
+        	seen = set()
+        	dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+	        while queue:
+            		x, y, d = queue.popleft()
+
+            		if (x, y) in seen:
+                		continue
+
+            		n_fresh -= min(1, d)
+            		seen.add((x, y))
+
+            		max_d = max(max_d, d)
+
+            		for dx, dy in dirs:
+                		nx, ny = x + dx, y + dy
+
+                		if (nx >= 0 and nx < R 
+				    and ny >= 0 and ny < C 
+				    and grid[nx][ny] == 1):
+                    			queue.append((nx, ny, d + 1))
+
+        	return max_d if n_fresh == 0 else -1
