27d243aa59ba942308889f79b7c14983028ef684
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-22 20:14:07 +0800

    House Money Collection

--- neetcode/1d_dp.py
+++ neetcode/1d_dp.py
@@ -23,3 +23,58 @@ class Solution:
             		prev, ways = ways, prev + ways
         	
 		return ways
+
+	def minCostClimbingStairs(self, cost: List[int]) -> int:
+		'''Minimum cost to climb stairs
+
+  		Space Complexity: O(n) -> Cost Array
+    		Time Complexity: O(n) -> Single Pass
+
+      		Args:
+			cost (list): Iterable of costs
+
+   		Returns:
+     			min_cost (int): Minimum cost to climb stairs
+		'''
+        	out = [0] * (len(cost) + 1)
+        	
+		for i in range(2, len(cost) + 1):
+            		out[i] = min(cost[i-1] + out[i-1], cost[i-2] + out[i-2])
+        	return out[-1]
+
+	def rob(self, nums: List[int]) -> int:
+		'''Maximum money obtained from non-consecutive houses
+
+  		Space Complexity: O(n) -> Money Array
+    		Time Complexity: O(n) -> Single Pass
+
+		Args:
+  			nums (list): Iterable of money available
+
+     		Returns:
+       			max_money (int): Maximum money obtainable
+	  	'''
+        	if len(nums) < 2:
+            		return max(nums) if nums else 0
+        
+		money = [0] * len(nums)
+        	money[0] = nums[0]
+        	money[1] = max(nums[:2])
+        
+		for i in range(2, len(nums)):
+            		money[i] = max(money[i-1], money[i-2] + nums[i])
+        	return money[-1]
+
+	def rob_round(self, nums: List[int]) -> int:
+		'''Maximum money obtained from circular arrangement of houses
+
+  		Space Complexity: O(n) -> Money Array
+    		Time Complexity: O(n) -> Single Pass (Twice)
+
+      		Args:
+			nums (list): Iterable of available money
+
+   		Returns:
+     			max_money (int): Maximum money obtained
+		'''
+        	return max(nums[0], self.rob(nums[1:]), self.rob(nums[:-1]))
