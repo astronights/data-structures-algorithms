b490b466110a2ef51b3e834d6c1b42f354b58a9e
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-22 22:48:09 +0800

    Longest common subsequence

--- neetcode/2d_dp.py
+++ neetcode/2d_dp.py
@@ -30,3 +30,29 @@ class Solution:
                 		dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
 
         	return dp[-1][-1]
+
+	def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+		'''Longest Common Subsequence between 2 words
+
+  		Space Complexity: O(m * n) -> DP Array
+    		Time Complexity: O(m * n) -> Single Pass
+
+      		Values: Length of word 1 (m), length of word 2 (n)
+
+      		Args:
+			text1 (str): Word 1
+   			text2 (str): Word 2
+
+      		Returns:
+			longest (int): Length of longest common subsequence
+   		'''
+        	m, n = len(text1), len(text2)
+        	dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        	for i in range(1, m + 1):
+            		for j in range(1, n + 1):
+                		if text1[i - 1] == text2[j - 1]:
+                    			dp[i][j] = 1 + dp[i - 1][j - 1]
+                		else:
+                    			dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        	return dp[-1][-1]
