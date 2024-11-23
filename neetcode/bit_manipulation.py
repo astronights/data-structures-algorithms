c740e5a0d6325b306a55882fa89f38b3cb088412
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-23 15:24:47 +0800

    Hamming Weight

--- neetcode/bit_manipulation.py
+++ neetcode/bit_manipulation.py
@@ -19,3 +19,21 @@ class Solution:
         	for n in nums:
             		x ^= n
         	return x
+
+	def hammingWeight(self, n: int) -> int:
+		'''Count number of 1 bits
+
+  		Space Complexity: O(1) -> Counter
+    		Time Complexity: O(n) -> Single Pass
+
+      		Args:
+			n (int): Number
+
+   		Returns:
+     			c (int): Number of one bits
+		'''
+        	c = 0
+        	while n:
+            		n = n & (n - 1)
+            		c += 1
+        	return c
