747a08fa0febada40cf347be14adce3bb5ae9443
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-23 10:06:35 +0800

    Distinct subsequences

--- neetcode/2d_dp.py
+++ neetcode/2d_dp.py
@@ -239,3 +239,31 @@ class Solution:
                 		dfs(i, j, -1)
 
         	return max(dp.values())
+
+	def numDistinct(self, s: str, t: str) -> int:
+		'''Number of distinct subsequences
+
+  		Space Complexity: O(m * n) -> DP Array
+    		Time Complexity: O(m * n) -> Single Pass
+
+      		Values: Length of s (m), Length of t (n)
+
+ 		Args:
+   			s (str): Iterable of characters
+      			t (str): Target sequence
+
+  		Returns:
+    			n_distinct (int): Number of distinct subsequences
+       		'''
+        	m, n = len(s), len(t)
+        	dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        	for i in range(m + 1):
+            		dp[i][0] = 1
+
+        	for i in range(1, m + 1):
+            		for j in range(1, n + 1):
+                		if s[i - 1] == t[j - 1]:
+                    			dp[i][j] += dp[i - 1][j - 1]
+                		dp[i][j] += dp[i - 1][j]
+        	return dp[-1][-1]
