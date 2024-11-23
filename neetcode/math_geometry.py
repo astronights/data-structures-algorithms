1afec00134044bdf46e5d6055e145fc5ea1ac634
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-11-23 20:45:58 +0800

    Spiral Matrix

--- neetcode/math_geometry.py
+++ neetcode/math_geometry.py
@@ -19,3 +19,34 @@ class Solution:
         	for i in range(len(matrix)):
             		matrix[i] = matrix[i][::-1]
         
+	def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+		'''Spiral matrix numbers
+
+  		Space Complexity: O(m * n) -> Output values
+    		Time Complexity: O(m * n) -> Matrix traversal
+
+      		Values: Matrix dimensions (m x n)
+
+ 		Args:
+   			matrix (list): Matrix of values
+
+      		Returns:
+			out (list): List of spiral values
+   		'''
+        	out = []
+        	dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        	R, C = len(matrix), len(matrix[0])
+        	x, y, c = 0, -1, 0
+
+        	while len(out) < R * C:
+            		dx, dy = dirs[c]
+            		x, y = x + dx, y + dy
+            		
+			out.append(matrix[x][y])
+            		matrix[x][y] = None
+
+            		nx, ny = x + dx, y + dy
+            		if nx < 0 or nx >= R or ny < 0 or ny >= C or matrix[nx][ny] is None:
+                		c = (c + 1) % 4
+        	return out
